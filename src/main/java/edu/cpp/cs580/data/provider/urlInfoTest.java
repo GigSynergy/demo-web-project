2ac471fd332456849e60da2ea81ea4dbce2f1b49
Shubhangi Shimpi <shimpishubhangi@yahoo.com>
2017-02-18 13:37:53 -0800

    Updated urlInfotest.java

--- src/main/java/edu/cpp/cs580/data/provider/urlInfoTest.java
+++ src/main/java/edu/cpp/cs580/data/provider/urlInfoTest.java
@@ -1,10 +1,8 @@
 package edu.cpp.cs580.data.provider;
 import java.util.List;
-
 import org.junit.Assert;
 import org.junit.Test;
 import edu.cpp.cs580.data.UrgentCareInfo;
-
 public class urlInfoTest 
 {
 	
