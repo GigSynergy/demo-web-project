61ac04e32d6e3f5d91abbc229d69839a3fc8d794
Tannaz Rezaei <tannazrezaei@yahoo.com>
2017-02-27 10:48:55 -0800

    Patient.java , form.html and home.html are changed.

--- src/main/java/edu/cpp/cs580/data/Patient.java
+++ src/main/java/edu/cpp/cs580/data/Patient.java
@@ -7,8 +7,7 @@ public class Patient
 	private String middleName;
 	private String lastName;
 	private String phoneNumber;
-	private String date;
-	private String time;
+	
 	private String facilityID;
 	private int queueNumber;
 	
@@ -45,22 +44,7 @@ public class Patient
 		this.phoneNumber = phoneNumber;
 	}
 	
-	public String getDate(){
-		return date;
-	}
-	
-	public void setDate(String date){
-		this.date = date;
-	}
 
-	
-	public String getTime(){
-		return time;
-	}
-	
-	public void setTime(String time){
-		this.time = time;
-	}
 	
 	public String getFacilityID()
 	{
