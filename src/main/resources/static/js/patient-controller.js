ec87e16899124ad49d11de0347cd8604f791b944
Shubhangi Shimpi <shimpishubhangi@yahoo.com>
2017-03-04 16:40:45 -0800

    clean up code

--- src/main/resources/static/js/patient-controller.js
+++ src/main/resources/static/js/patient-controller.js
@@ -3,12 +3,12 @@ angular.module('cs580App',[])
 angular.module('cs580App')
 	
 	.controller('patient-controller',['$scope','$http',function($scope,$http) {
-
+	
+    	
     $scope.save = function() 
     {
-    	console.log($scope.patient);
-    	//JSON.stringify($scope.patient);
     	
+    	console.log($scope.patient);
     	return $http.post("/cs580/patient",$scope.patient);
     };
 }])
